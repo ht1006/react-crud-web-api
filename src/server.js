c7f5ef8613ca87a246dbe7e5aa13c088e72cc915
ht1006 <36884859+ht1006@users.noreply.github.com>
2020-05-28 19:53:42 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8081;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
