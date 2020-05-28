f5fc885b89745f53b1212a1417ef28bf0b4db490
ht1006 <36884859+ht1006@users.noreply.github.com>
2020-05-28 19:14:39 +0100

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8081;
+
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+export default axios.create({
+    baseURL: server_ip_address + ", server_port " + server_port,
+    headers: {
+        "Content-type": "application/json"
+    }
+});
