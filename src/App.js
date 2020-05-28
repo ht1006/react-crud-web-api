59ac839f1dcda3d77c705670a99c03106cc3c635
ht1006 <36884859+ht1006@users.noreply.github.com>
2020-05-28 19:14:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,6 @@ import AddTutorial from "./components/add-tutorial.component";
 import Tutorial from "./components/tutorial.component";
 import TutorialsList from "./components/tutorials-list.component";
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
- 
-
-server.listen(server_port, server_ip_address, function () {
-
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-
-});
-
 class App extends Component {
   render() {
     return (
