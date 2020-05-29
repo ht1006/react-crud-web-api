a264309376762240f1b0251da7f9cf4f22dde5db
ht1006 <36884859+ht1006@users.noreply.github.com>
2020-05-29 13:08:25 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 export default axios.create({
-    baseURL: "http://" + server_ip_address + ":" + server_port,
+    baseURL: "http://" + server_ip_address + ":" + server_port +  "/api",
     headers: {
         "Content-type": "application/json"
     }
